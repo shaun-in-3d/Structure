3947f23c0f31a6fc22960a621e745fb88d4d1ac9
Rebecca <146950501+Azaersyss@users.noreply.github.com>
2024-03-15 13:54:41 +0000

    Wwise Additions

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,14 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AMBIENCE_BIRDS = 3850837052U;
+        static const AkUniqueID AMBIENCE_CAFE = 3865913715U;
+        static const AkUniqueID AMBIENCE_WIND = 156328838U;
+        static const AkUniqueID BIKE_BELL = 1766085546U;
+        static const AkUniqueID BIKE_SURFACE = 901759366U;
         static const AkUniqueID CANCEL = 2760337059U;
         static const AkUniqueID CONFIRM = 1517537939U;
+        static const AkUniqueID EAT_TEA = 3910981062U;
         static const AkUniqueID GAME_LOSS = 3425053579U;
         static const AkUniqueID PHONE_NOTI = 3042867002U;
     } // namespace EVENTS
@@ -45,8 +51,8 @@ namespace AK
             namespace SWITCH
             {
                 static const AkUniqueID GRASS = 4248645337U;
-                static const AkUniqueID PAVEMENT = 2830102203U;
                 static const AkUniqueID ROAD = 2110808655U;
+                static const AkUniqueID SAND = 803837735U;
             } // namespace SWITCH
         } // namespace BIKE_SURFACE
 
@@ -62,6 +68,11 @@ namespace AK
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID OUTDOORS_REVERB = 2653823909U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
